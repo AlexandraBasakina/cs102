43f6d1a42f46f55c77bbd43d3a89a16058140e5e
AlexandraBasakina <alexandra.basakina.2001@gmail.com>
2019-10-05 14:17:11 +0300

    Добавлено решение первой части для шифра Цезаря

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,26 +1,38 @@
-def encrypt_caesar(plaintext: str) -> str:
-    for i in range (len(plaintext)):
-        znach=ord(plaintext[i])
-        if znach >= 65 and znach <= 87 or znach >= 97 and znach <= 119:
-            znach += 3
-        elif znach >= 88 and znach <= 90 or znach >= 119 and znach <= 122:
-            znach -= 23
-        print(chr(znach), end='')
-    return chr(32)
-        
-slovodliakod=str(input())
-print(encrypt_caesar(slovodliakod))
+def encrypt_caesar(plaintext):
+    """
+    Encrypts plaintext using a Caesar cipher.
+    >>> encrypt_caesar("PYTHON")
+    'SBWKRQ'
+    >>> encrypt_caesar("python")
+    'sbwkrq'
+    >>> encrypt_caesar("Python3.6")
+    'Sbwkrq3.6'
+    >>> encrypt_caesar("")
+    ''
+    """
+    ciphertext = ''
+    for ch in plaintext:
+        code = ord(ch)
+        if 'a' <= ch < 'x' or 'A' <= ch < 'X':
+            code += 3
+        elif 'x' <= ch <= 'z' or 'X' <= ch <= 'Z':
+            code -= 23
+        ciphertext += chr(code)
+    return ciphertext
 
 
-#encrypt_caesar("PYTHON") выводит на экран "SBWKRQ"
-#encrypt_caesar("python") выводит на экран "sbwkrq"
-#encrypt_caesar("Python3.6") выводит на экран "Sbwkrq3.6"
-#encrypt_caesar("") выводит на экран ""
-
-
-
-
-def decrypt_caesar(ciphertext: str) -> str:
+def decrypt_caesar(ciphertext):
+    """
+    Decrypts a ciphertext using a Caesar cipher.
+    >>> decrypt_caesar("SBWKRQ")
+    'PYTHON'
+    >>> decrypt_caesar("sbwkrq")
+    'python'
+    >>> decrypt_caesar("Sbwkrq3.6")
+    'Python3.6'
+    >>> decrypt_caesar("")
+    ''
+    """
     for i in range (len(ciphertext)):
         znach=ord(ciphertext[i])
         if znach >= 68 and znach <= 90 or znach >= 100 and znach <= 122:
@@ -29,14 +41,3 @@ def decrypt_caesar(ciphertext: str) -> str:
             znach += 23
         print(chr(znach), end='')
     return chr(32)
-    
-
-zacodslovodliarazkod=str(input())
-print(decrypt_caesar(zacodslovodliarazkod))
-
-
-
-#decrypt_caesar("SBWKRQ") выводит на экран "PYTHON"
-#decrypt_caesar("sbwkrq") выводит на экран "python"
-#decrypt_caesar("Sbwkrq3.6") выводит на экран "Python3.6"
-#decrypt_caesar("") выводит на экран ""
