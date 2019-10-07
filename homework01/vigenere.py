0c0aab488c31289657d0ab2d6ee180a88292efbc
AlexandraBasakina <alexandra.basakina.2001@gmail.com>
2019-10-07 13:51:10 +0300

    Добавлено новое решение шифр Виженера

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,48 +1,57 @@
-def encrypt_vigenere(plaintext: str, key: str) -> str:
-    key_len = len(key)
+def encrypt_vigenere(plaintext, keyword):
+    """
+    Encrypts plaintext using a Vigenere cipher.
+    >>> encrypt_vigenere("PYTHON", "A")
+    'PYTHON'
+    >>> encrypt_vigenere("python", "a")
+    'python'
+    >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
+    'LXFOPVEFRNHR'
+    """
+    key_len = len(keyword)
     plaintext_len = len(plaintext)
     while key_len != plaintext_len:
-        key += key
-        key_len = len(key)
+        keyword += keyword
+        key_len = len(keyword)
         if key_len > plaintext_len:
-            key = key[:plaintext_len]
-            key_len = len(key)
-    key_as_int = [ord(i) for i in key]
-    plaintext_int = [ord(i) for i in plaintext]
-    ciphertext = ''
-    for i in range(len(plaintext_int)):
-        znach = (plaintext_int[i] + key_as_int[i % key_len]) % 26
-        ciphertext += chr(znach + 65)
+            keyword = keyword[:plaintext_len]
+            key_len = len(keyword)
+    if keyword.isupper():
+        code_A, code_Z = 65, 90
+    else:
+        code_A, code_Z = 97, 122
+    for ch, k in zip(list(plaintext), list(keyword)):
+        if ord(ch) + ord(k) - code_A > code_Z:
+            ch = chr((ord(ch)) - 26)
+        ciphertext += chr(ord(ch) + (ord(k) - code_A))
     return ciphertext
     
-plaintext=str(input())
-key=str(input())
-print(encrypt_vigenere(plaintext, key))
 
-#encrypt_vigenere(ATTACKATDAWN, LEMON) выводит на экран LXFOPVEFRNHR
-
-
-
-
-def decrypt_vigenere(ciphertext: str, key: str) -> str:
-    key_len = len(key)
+def decrypt_vigenere(ciphertext, keyword):
+    """
+    Decrypts a ciphertext using a Vigenere cipher.
+    >>> decrypt_vigenere("PYTHON", "A")
+    'PYTHON'
+    >>> decrypt_vigenere("python", "a")
+    'python'
+    >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
+    'ATTACKATDAWN'
+    """
+    key_len = len(keyword)
     ciphertext_len = len(ciphertext)
     while key_len != ciphertext_len:
-        key += key
-        key_len = len(key)
+        keyword += keyword
+        key_len = len(keyword)
         if key_len > ciphertext_len:
-            key = key[:ciphertext_len]
-            key_len = len(key)
-    key_as_int = [ord(i) for i in key]
-    ciphertext_int = [ord(i) for i in ciphertext]
-    plaintext = ''
-    for i in range(len(ciphertext_int)):
-        znach = (ciphertext_int[i] - key_as_int[i % key_len]) % 26
-        plaintext += chr(znach + 65)
+            keyword = keyword[:ciphertext_len]
+            key_len = len(keyword)
+    if keyword.isupper():
+        code_A = 65
+    else:
+        code_A = 97
+    for ch, k in zip(list(ciphertext), list(keyword)):
+        if (ord(ch) < ord(k)):
+            ch = chr(ord(ch) + 26)
+        plaintext += chr(ord(ch) - (ord(k) - code_A))
     return plaintext
-    
-ciphertext=str(input())
-key=str(input())
-print(decrypt_vigenere(ciphertext, key))
-
-#decrypt_vigenere(LXFOPVEFRNHR, LEMON) выводит на экран ATTACKATDAWN
+ 
