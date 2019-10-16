5bd0a36a5f0d88c07f5e746b1e8bf13b9a547982
AlexandraBasakina <alexandra.basakina.2001@gmail.com>
2019-10-16 12:42:54 +0300

    Добавлено решение для шифра RSA, проверенное в pep8

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -1,50 +1,117 @@
-p = int(input())
-q = int(input())
+import random
+
+
 def is_prime(n: int) -> bool:
-    delitel = 2
-    koldel = 1
-    while delitel*delitel <= n and n % delitel == 0:
-        koldel+=1
-    if koldel > 1:
+    """
+    Tests to see if a number is prime.
+    >>> is_prime(2)
+    True
+    >>> is_prime(11)
+    True
+    >>> is_prime(8)
+    False
+    """
+    divider = 2
+    numdividers = 1
+    while divider*divider <= n and n % divider == 0:
+        numdividers += 1
+        divider += 1
+    if numdividers > 1:
         return False
     else:
         return True
-    pass
 
-def generate_keypair(p: int, q: int) -> Tuple[Tuple[int, int], Tuple[int, int]]:
+
+def gcd(a, b):
+    """
+    Euclid's algorithm for determining the greatest common divisor.
+    >>> gcd(12, 15)
+    3
+    >>> gcd(3, 7)
+    1
+    """
+    while b != 0:
+        (a, b) = (b, a % b)
+        return a
+
+
+def multiplicative_inverse(e, phi):
+    """
+    Euclid's extended algorithm for finding the multiplicative
+    inverse of two numbers.
+    >>> multiplicative_inverse(7, 40)
+    23
+    """
+    def euclex(a, b):
+        if a == 0:
+            return (b, 0, 1)
+        else:
+            g, y, x = euclex(b % a, a)
+            return (g, x - (b // a) * y, y)
+    g, x, y = euclex(a, m)
+    if g != 1:
+        raise Exception('Modular inverse does not exist')
+    else:
+        return x % m
+
+
+def generate_keypair(p, q):
     if not (is_prime(p) and is_prime(q)):
         raise ValueError('Both numbers must be prime.')
     elif p == q:
         raise ValueError('p and q cannot be equal')
 
-    elif (is_prime(p) and is_prime(q)):
-         n = pq
-         phi = (p-1)(q-1)
+    n = pq
+    phi = (p-1)*(q-1)
 
-    def gcd(e: int, phi: int) -> int:
-        while gsd!=1:
-            e = random.randrange(1, phi)
-            e1=e
-            if phi-e == 1:
-                gsd = 1
-            else:
-                if phi > e:
-                    while phi%e != 0 or phi!=e or phi==0 or e==0:
-                        phi = phi-e
-                        gsd = phi
-                else:
-                    while e%phi != 0 or phi!=e or phi==0 or e==0:
-                        e = e-phi
-                        gsd = e
-                        
-            if phi == 1 and e == 1 or gsd == 1:
-                e = e1 
-    pass       
-    d=0
-    while d*(e%phi)!=1:
-        d1 = random.randrange(1, phi)
-        if d1 != e:
-            d = d1
-        else:
-            d=0
+    # Choose an integer e such that e and phi(n) are coprime
+    while not (is_prime(e) == True):
+        e = random.randrange(1, phi)
+
+    # Use Euclid's Algorithm to verify that e and phi(n) are comprime
+    g = gcd(e, phi)
+    while g != 1:
+        e = random.randrange(1, phi)
+        g = gcd(e, phi)
+
+    # Use Extended Euclid's Algorithm to generate the private key
+    d = multiplicative_inverse(e, phi)
+
+    # Return public and private keypair
+    # Public key is (e, n) and private key is (d, n)
     return ((e, n), (d, n))
+
+
+def encrypt(pk, plaintext):
+    # Unpack the key into it's components
+    key, n = pk
+    # Convert each letter in the plaintext to numbers based on
+    # the character using a^b mod m
+    cipher = [(ord(char) ** key) % n for char in plaintext]
+    # Return the array of bytes
+    return cipher
+
+
+def decrypt(pk, ciphertext):
+    # Unpack the key into its components
+    key, n = pk
+    # Generate the plaintext based on the ciphertext and key using a^b mod m
+    plain = [chr((char ** key) % n) for char in ciphertext]
+    # Return the array of bytes as a string
+    return ''.join(plain)
+
+
+if __name__ == '__main__':
+    print("RSA Encrypter/ Decrypter")
+    p = int(input("Enter a prime number (17, 19, 23, etc): "))
+    q = int(input("Enter another prime number (Not one you entered above): "))
+    print("Generating your public/private keypairs now . . .")
+    public, private = generate_keypair(p, q)
+    print("Your public key is ", public, " and your private key is ", private)
+    message = input("Enter a message to encrypt with your private key: ")
+    encrypted_msg = encrypt(private, message)
+    print("Your encrypted message is: ")
+    print(''.join(map(lambda x: str(x), encrypted_msg)))
+    print("Decrypting message with public key ", public, " . . .")
+    print("Your message is:")
+    print(decrypt(public, encrypted_msg))
