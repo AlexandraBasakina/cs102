d1ec95a2c12f641d35edbf8e24e4820303227014
AlexandraBasakina <56187112+AlexandraBasakina@users.noreply.github.com>
2019-12-13 12:09:06 +0300

    Загрузила чёртову программу.
    АЛЛИЛЛУЯ~

---
+++ homework04/config.py
@@ -0,0 +1,5 @@
+access_token = "54589b912986f94fb432cdbda93ce1ea10dbebc566d82238369645ac53c4b105d8808cb0807d21f678e86"
+domain = "https://api.vk.com/method"
+user_id = 299955321
+fields = 'sex'
+v = '5.103'
